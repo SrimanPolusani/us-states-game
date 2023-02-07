fbabe8b74d5250f5ca55c92cd403917af3e2549c
SrimanPolusani <srimanpolusani1@gmail.com>
2023-02-07 18:29:24 +0530

    Add few comments, shorten var name and remove unnecessary code

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+# <---- Import Statements ---->
 from turtle import Turtle, Screen
 import pandas
 from states_name_display import States_Name_Display
-from detecting_repeated_answers import detect_repeated_ans
+from repeated_answers import detect_repeated_ans
 
+# <---- Constants ---->
+TOTAL_STATES = 50
+
+# <---- Initializing Turtle Object and the Screen ---->
 turtle = Turtle()
 screen = Screen()
 screen.title("U.S STATES GAME")
@@ -12,41 +17,50 @@ turtle.shape(image)
 writing_turtle = States_Name_Display()
 
 
-def take_input(numb_of_correct_ans, numb_of_total_sates):
-    answer = screen.textinput(title=f"{numb_of_correct_ans}/{numb_of_total_sates} are correct",
+# <---- Dialog Box Function ---->
+def dialog_box(correct_ans):
+    """"
+    This function takes no of correct answers and total no of states as augments, displays dialog box and returns received input from the user
+    """
+    answer = screen.textinput(title=f"{correct_ans}/{TOTAL_STATES} are correct",
                               prompt="Enter a state name?").lower()
     return answer
 
 
+# 50_states.csv states and their positions on the image black_states_img.gif
 states_data = pandas.read_csv("50_states.csv")
-all_states_lst = states_data.state.to_list()
-all_x_values = states_data.x.to_list()
-all_y_values = states_data.y.to_list()
 
-no_of_correct_ans = 0
+# Converting data into a lists
+states_lst = states_data.state.to_list()
+x_values = states_data.x.to_list()
+y_values = states_data.y.to_list()
+
+count_correct_ans = 0
 input_ans_lst = []
 game_is_on = True
 
 while game_is_on:
-    answered_state = take_input(no_of_correct_ans, 50)
-    for state in all_states_lst:
-        if state.lower() == answered_state:
-            repeated = detect_repeated_ans(input_ans_lst, answered_state)
+
+    # Taking answer from the user
+    answer = dialog_box(count_correct_ans)
+    for state in states_lst:
+        if state.lower() == answer:
+
+            # Checking if it's a repeated answer
+            repeated = detect_repeated_ans(input_ans_lst, answer)
             if repeated:
                 break
             elif repeated is None:
-                x_y_values_index = all_states_lst.index(state)
-                x_value = all_x_values[x_y_values_index]
-                y_value = all_y_values[x_y_values_index]
-                input_ans_lst.append(answered_state)
+                state_index = states_lst.index(state)
+                x_value = x_values[state_index]
+                y_value = y_values[state_index]
+                input_ans_lst.append(answer)
                 writing_turtle.write(x_value, y_value, state)
-                no_of_correct_ans += 1
-                total_no_of_states = len(all_states_lst)
-                screen.title(f"{no_of_correct_ans}/{total_no_of_states} are correct")
-                at_least_one_element = True
+                count_correct_ans += 1
 
     if len(input_ans_lst) == 50:
         print("YOU WON THE GAME!")
+        print("Game Over")
         game_is_on = False
 
 screen.exitonclick()
